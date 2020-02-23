1bb335e5e7feead0c081537da4bbe481e39a7b98
bader_mazro <bader.mazro@gmail.com>
2020-02-23 13:46:18 +0300

    adding app.js and basic set up axios api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Addweather from './components/Addweather';
+import WDATA from './WDATA'
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      api_key:WDATA.access_key,
+      weather:[],
+      city:"Riyadh",
+      newCity:'',
+    }
+
+  }
+  getWeatherData = () =>{
+
+    const url =`http://api.weatherstack.com/current?access_key=${this.state.api_key}&query=${this.state.city}`
+    axios({
+      methos:"GET",
+      url:url,
+    }).then(response=>{
+      console.log(response)
+    });
+  };
+
+  handleCity=e=>{
+    this.setState({city:e.target.value})
+    
+    
 }
 
-export default App;
+
+
+  render(){
+    console.log(this.state.city)
+    // console.log(WDATA.access_key)
+    // console.log(this.state.api_key)
+     console.log(this.getWeatherData())
+  
+
+    return(
+      
+    <div>
+      
+      <h1>Weather data</h1>
+      <Addweather city={this.state.city}
+      handleCity={this.handleCity}
+      handleClick={this.handleClick}/>
+    </div>)
+  }
+
+}
