73e514144c555129d6f48de75778caa240aa7267
bader_mazro <bader.mazro@gmail.com>
2020-02-26 16:05:01 +0300

    Updates

---
+++ precache-manifest.dad88ae3dc75e94b74873023ab2ec0c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82e80ffa4bba3f817aead74b7f634be4",
+    "url": "/project-2/index.html"
+  },
+  {
+    "revision": "3bfad5bac4844289f560",
+    "url": "/project-2/static/css/main.42ab7794.chunk.css"
+  },
+  {
+    "revision": "4e091c61b489c4ccbd4d",
+    "url": "/project-2/static/js/2.672cf43d.chunk.js"
+  },
+  {
+    "revision": "192201c36557064462a543b746126438",
+    "url": "/project-2/static/js/2.672cf43d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bfad5bac4844289f560",
+    "url": "/project-2/static/js/main.b4b9ce69.chunk.js"
+  },
+  {
+    "revision": "7ad67d443d39c97fc39d",
+    "url": "/project-2/static/js/runtime-main.681a74c5.js"
+  }
+]);
